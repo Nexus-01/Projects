37889d2c6833ed8026851836da11e05d7e2a329c
Nexus-01 <137302676+Nexus-01@users.noreply.github.com>
2024-01-05 00:02:39 -0500

    Update createCSV.py
    Will modify to add the features mentioned in the comments. Testing will be done and confirmed before pushing to main.

--- ActiveDirectory/UserCreation/createCSV.py
+++ ActiveDirectory/UserCreation/createCSV.py
@@ -3,7 +3,7 @@ import csv
 
 # open .csv file in write mode and set as 'userFile'
 with open('usersTest.csv', mode='w') as userFile:
-	# create an array name titleFields that contains the item names
+	# create an array named titleFields that contains the item names
 	# then use the DictWriter method to use the dictionary structure for item:key pairs
 	titleFields = ['firstName', 'lastName', 'department']
 	contentWriter = csv.DictWriter(userFile, fieldnames=titleFields)
@@ -13,3 +13,10 @@ with open('usersTest.csv', mode='w') as userFile:
 	contentWriter.writerow({'firstName':'John', 'lastName':'Smith', 'department':'IT'})
 	contentWriter.writerow({'firstName':'Jane', 'lastName':'Doe', 'department':'Engineering'})
 	contentWriter.writerow({'firstName':'Test', 'lastName':'', 'department':'HR'})
+
+
+## TODO:
+#	Create a generator for random zip codes and add the zip code header. Assign each user a random zip code.
+#	Do the same as the zip code generate and header, but for phone numbers.
+#	Perform checks to ensure duplicate names don't occur. Having duplicates will result in the powershell script failing to add the duplicate user due to having the same names.
+#	More to come after the above is completed.
