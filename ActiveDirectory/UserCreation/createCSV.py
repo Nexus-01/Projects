265a05a41fdc0a721d4ba134b33960c5f323740b
Nexus-01 <137302676+Nexus-01@users.noreply.github.com>
2024-03-31 14:44:30 -0700

    Update createCSV.py
    Now reads name from a provided name file, and randomly selects a department from an array. 
    Decent for testing, not for production. Production will have a different process altogether for CSV files or similar. This is solely for testing.
    Tested and confirmed working. Committing to main.

--- ActiveDirectory/UserCreation/createCSV.py
+++ ActiveDirectory/UserCreation/createCSV.py
@@ -1,15 +1,33 @@
-#import csv library
+#import csv, random
 import csv
+import random
 
-# open .csv file in write mode and set as 'userFile'
-with open('usersTest.csv', mode='w') as userFile:
-	# create an array name titleFields that contains the item names
-	# then use the DictWriter method to use the dictionary structure for item:key pairs
-	titleFields = ['firstName', 'lastName', 'department']
-	contentWriter = csv.DictWriter(userFile, fieldnames=titleFields)
+# open .txt file containing first/last names and set as 'usernames'
+with open('names.txt', mode='r') as userNames:
+	names = userNames.readlines()
+	names = [x.strip() for x in names]
+	# open .csv file in write mode and set as 'userFile'
+	
+	with open('usersTest.csv', mode='w') as userFile:
+		# create an array name titleFields that contains the item names
+		# then use the DictWriter method to use the dictionary structure for item:key pairs
+		titleFields = ['firstName', 'lastName', 'department']
+		contentWriter = csv.DictWriter(userFile, fieldnames=titleFields)
+		
+ 		# write the headers 
+		contentWriter.writeheader()
 
-	# write the headers, then create a new row containing each user's item:key pair
-	contentWriter.writeheader()
-	contentWriter.writerow({'firstName':'John', 'lastName':'Smith', 'department':'IT'})
-	contentWriter.writerow({'firstName':'Jane', 'lastName':'Doe', 'department':'Engineering'})
-	contentWriter.writerow({'firstName':'Test', 'lastName':'', 'department':'HR'})
+		# test list for departments
+		departments = ['IT', 'HR', 'IAM', 'Admins', 'Billings', 'Finance']
+		
+		#set first and last names for each user
+		for name in names:
+			firstname = name.split(" ")[0]
+			lastname = name.split(" ")[1]
+			department = random.choice(departments)
+			
+			# create a new row containing each user's item:key pair
+			contentWriter.writerow({'firstName': firstname, 'lastName': lastname, 'department':department})
+			
+
+
